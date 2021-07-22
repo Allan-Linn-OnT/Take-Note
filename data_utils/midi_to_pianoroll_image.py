ff9c13563ee8339bc1c0daa2088076283a585859
Martin <martin.ffrench@mail.utoronto.ca>
2021-07-21 20:12:07 -0400

    added image decode tools

--- data_utils/midi_to_pianoroll_image.py
+++ data_utils/midi_to_pianoroll_image.py
@@ -5,38 +5,10 @@ from note_seq.sequences_lib import quantize_note_sequence
 from PIL import Image
 import numpy as np
 from copy import deepcopy
+from tqdm import tqdm
 
 ped = PianorollEncoderDecoder(input_size=121)
-OneHotEncoderDecoder = MelodyOneHotEncoding(0, 128)
-num_classes = OneHotEncoderDecoder.num_classes
-
-def mid_file_to_melody(mid_file_path, quant=1, num_secs=10):
-    '''
-    converts a file path to a ns.Melody() object, need to specify the number of counts given to a quarter note for note_sequence quantization
-
-    Args:
-        - mid_file_path - str, path to the mid file, can be absolute or relative
-        - quant - number of steps a quarter note represents, this is required for quantization, bigger number means more data but better note accuracy
-        - num_secs - the max number of seconds of music we are interested in listening to, if set to 0, we will not truncate
-    '''
-    f_ns = ns.midi_file_to_note_sequence(mid_file_path)
-
-    main_instrument = ns.infer_melody_for_sequence(f_ns)
-    notes = [note for note in f_ns.notes if note.instrument == main_instrument]
-
-    del f_ns.notes[:]
-    f_ns.notes.extend(
-        sorted(notes, key=lambda note: note.start_time))
-
-    melon = ns.Melody()
-    melon.from_quantized_sequence(quantize_note_sequence(f_ns, quant), instrument=main_instrument, ignore_polyphonic_notes=True)
-
-    if num_secs:
-        if melon.total_time > num_secs:
-            melon = ns.extract_subsequence(
-                melon, 0, num_secs)
-
-    return melon
+QUANT = 4
 
 def mid_file_to_pr_input_target(mid_file_path, quant=4, num_secs=0):
     '''
@@ -94,84 +66,19 @@ def mid_file_to_pianoroll(mid_file_path, quant=4, num_secs=0, top_only=True):
 
     return proll
 
-def convert_to_onehot(ns_melody, seq_len=0):
-    '''
-    convert note_seq.Melody() object to onehot numpy array of length seq_len
-
-    Args:
-        ns_melody - note_seq.Melody() object created from midi notes
-        seq_len - number of columns for desired sequence, if zero, just use encoded melody length
-    Returns:
-        np.array - dimension of 130 x seq_len
-    '''
-
-    onehot_mel = [ OneHotEncoderDecoder.encode_event(hot) for hot in ns_melody]
-
-    if seq_len == 0:
-        seq_len = len(onehot_mel)
-
-    out = np.zeros((num_classes, seq_len))
-
-    len(ns_melody)
-    out[np.arange(onehot_mel.size), onehot_mel] = 1
-
-    return out
-
-def mid_to_png(mid_file_path, outfile, quant=4, rgb=False):
-    '''
-    convert a midi file to an png image
-
-    args:
-        - mid_file_path: str, path to the desired mid file to read
-        - outfile: str, name of the desired output image
-        - quant: note quantization
-        - rgb: add more channels with repeated melody
-    '''
-
-    convert_to_onehot(mid_file_to_melody(mid_file_path, quant=quant))
-
-midi_file = 'MuseData/test/bach.cant.0003_midip_02.mid'
-
-#f_ns = ns.midi_file_to_note_sequence(midi_file)
-#mel = ns.infer_melody_for_sequence(f_ns)
-#notes = [note for note in f_ns.notes if note.instrument == mel]
-#
-#del f_ns.notes[:]
-#f_ns.notes.extend(
-#    sorted(notes, key=lambda note: note.start_time))
-#melon = ns.Melody()
-#
-#q_ns = quantize_note_sequence(f_ns, 1)
-#
-#print(q_ns)
-#melon.from_quantized_sequence(q_ns, instrument=mel, ignore_polyphonic_notes=True)
-#
-#for ev in melon:
-#    print(ev)
-
-#melon = mid_file_to_melody(midi_file)
-#
-#onehot_mel = [ OneHotEncoderDecoder.encode_event(hot) for hot in melon]
-#
-#print(onehot_mel)
-#convert_back = [ OneHotEncoderDecoder.decode_event(hot) for hot in onehot_mel]
-#
-#print(convert_back)
 def mid_file_to_input_target(midi_file):
     '''
     convert midi file to 2d numpy array
     Use this for inference or when you only need model input
     '''
 
-    in_pr, tar_pr = mid_file_to_pr_input_target(midi_file)
+    in_pr, tar_pr = mid_file_to_pr_input_target(midi_file, quant=QUANT)
     input_mask = np.zeros((ped.input_size, in_pr.num_steps))
     target_mask = np.zeros((ped.input_size, tar_pr.num_steps))
     for step in in_pr.steps:
         input_mask[:, step] = ped.events_to_input(in_pr, step)
-
     for step in tar_pr.steps:
-       target_mask[:, step] = ped.events_to_input(tar_pr, step)
-
+        target_mask[:, step] = ped.events_to_input(tar_pr, step)
     return input_mask, target_mask
 
 def mid_file_to_arr(midi_file):
@@ -180,24 +87,45 @@ def mid_file_to_arr(midi_file):
     Use this for inference or when you only need model input
     '''
 
-    pr = mid_file_to_pianoroll(midi_file)
+    pr = mid_file_to_pianoroll(midi_file, quant=QUANT)
     mask = np.zeros((ped.input_size, pr.num_steps))
     for step in pr.steps:
         mask[:, step] = ped.events_to_input(pr, step)
     return mask
 
-def mid_file_to_image(midi_file):
-    '''
-    convert midi file to an image using the piano roll format
-    '''
-
-    # sample = mask[:,:,None]*np.ones(3,dtype=int)[None,None,:]
-    # sample = 255*sample.astype(int)
-    im = Image.fromarray(255*mid_file_to_arr(midi_file)).convert("RGB")
-    im.save("swagger.jpg")
-
 def arr_to_image(arr, out_path):
     Image.fromarray(255*arr).convert("RGB").save(out_path)
+
+def image_to_class_indices(image_path):
+    '''
+    convert image to class indices used by PED to decode into pianorollseq
+    '''
+    cids = []
+    img = np.asarray(Image.open(image_path).convert("RGB"))[:,:,0] # assume each channel is the same
+    for i in range(img.shape[1]):
+        pitches = np.where(img[:, i])[0]
+        val=0
+        for pitch in pitches:
+            val += 1<<int(pitch) # Kinda Hacky, but works, python facilitates inf shift left, but numpy only goes to 63, need to convert to pure python here
+        cids.append(val)
+    return cids
+
+def class_indices_to_pianoroll(cids):
+    '''
+    convert class indicies to pianorollseq
+    '''
+    events = []
+    for cid in cids:
+        events.append(ped.class_index_to_event(cid, []))
+    #events = [ped.class_index_to_event(cid, []) for cid in cids]
+    return ns.PianorollSequence(events_list=events, steps_per_quarter=QUANT,)
+
+def image2midi(image_path, out_dir, mods=['converted']):
+    '''
+    convert an image back into a midi file
+    use same name as initial file and place in converted folder
+    '''
+    ns.note_sequence_to_midi_file(class_indices_to_pianoroll(image_to_class_indices(image_path)).to_sequence(), f"{out_dir}/{mods[0]}/" + image_path.split("/")[-1].replace(".png",".mid"))
 
 def midi2image(midi_path, out_dir, mods=['input', 'target']):
     '''
@@ -214,9 +142,6 @@ if __name__ == "__main__":
     '''
     example usage
     python midi_to_pianoroll_image.py -m train -o train_input
-    TODO:
-     - Add parameter for number of seconds of midi to keep
-     - Add parameter for quantization of midi
     '''
 
     import argparse
@@ -228,26 +153,50 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--midi_path", type=str, help="Folder Source of Midi files")
-    parser.add_argument("-o", "--output_dir", type=str, help="Specify output dir for converted midi files ")
-    parser.add_argument("-s", "--single_path", type=str, help="For debug and testing, run converter on single image")
+    parser.add_argument("-o", "--output_dir", type=str, help="Specify output dir for converted midi files, if converting image to midi, this is output midi path")
+    parser.add_argument("-s", "--single_path", type=str, help="For debug and testing, run converter on single mid")
+    parser.add_argument("-d", "--single_image", type=str, help="For debug and testing, run decoder on single image")
+    parser.add_argument("-i", "--image_path", type=str, help="Folder Source for Image files")
     args = parser.parse_args()
 
     midi_path = args.midi_path
-    out_dir = args.output_dir
-    single = args.single_path
-    
-    if single is not None:
-        inp, tar = mid_file_to_input_target(single)
+    image_path = args.image_path
+
+    out = args.output_dir
+
+    single_encode = args.single_path
+    single_decode = args.single_image
+
+
+    if single_encode is not None:
+        inp, tar = mid_file_to_input_target(single_encode)
         arr_to_image(inp, 'debug_inp.png')
         arr_to_image(tar, 'debug_tar.png')
         exit(0)
-    os.makedirs(out_dir, exist_ok=True) # Make the folder to hold the output files
-    os.makedirs(out_dir + '/input', exist_ok=True)
-    os.makedirs(out_dir + '/target', exist_ok=True)
 
-    for midi_file in glob.glob(os.path.join(midi_path,"*.mid")):
-        print(midi_file)
-        try:
-            midi2image(midi_file, out_dir=out_dir)
-        except:
-            print(f"{midi_file} FAILED TO CONVERT!")
+    if single_decode is not None:
+        ns.note_sequence_to_midi_file(class_indices_to_pianoroll(image_to_class_indices(single_decode)).to_sequence(), out)
+        exit(0)
+
+    if image_path is not None:
+        os.makedirs(out, exist_ok=True) # Make the folder to hold the output files
+        os.makedirs(out + '/converted', exist_ok=True)
+
+        for image_file in tqdm(glob.glob(os.path.join(image_path,"*.png"))):
+            try:
+                image2midi(image_file, out_dir=out)
+            except:
+                print(f"{image_file} FAILED TO CONVERT!")
+
+        exit(0)
+
+    if midi_path is not None:
+        os.makedirs(out, exist_ok=True) # Make the folder to hold the output files
+        os.makedirs(out + '/input', exist_ok=True)
+        os.makedirs(out + '/target', exist_ok=True)
+
+        for midi_file in tqdm(glob.glob(os.path.join(midi_path,"*.mid"))):
+            try:
+                midi2image(midi_file, out_dir=out)
+            except:
+                print(f"{midi_file} FAILED TO CONVERT!")
