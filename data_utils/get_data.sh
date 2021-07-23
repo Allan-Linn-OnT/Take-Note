c26b8ba98fcca56812a4afc8aa313e4e75c9ed40
Martin <martin.ffrench@mail.utoronto.ca>
2021-07-22 22:23:48 -0400

    added bushgraft jazz dataset

--- data_utils/get_data.sh
+++ data_utils/get_data.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+IFS=$'\n'
 mkdir ../data
 find ../data/* -print0 | xargs -0 rm -rf # make sure the data directory is empty, don't save anything in there
 
@@ -12,16 +13,18 @@ unzip MuseData.zip
 unzip Piano-midi.de.zip
 unzip Nottingham.zip
 unzip "JSB Chorales.zip"
+tar -xvf bushgraft.tar.gz # part of the repo
 
 mv MuseData ../data
 mv Piano-midi.de ../data
 mv Nottingham ../data
 mv "JSB Chorales" ../data
+mv jazz ../data
 
 rm -rf *.zip
 
 # need to do some processing of MuseData, and Piano-midi.de
-dirs=("MuseData" "Piano-midi.de" "Nottingham")
+dirs=("MuseData" "Piano-midi.de" "Nottingham" "jazz")
 echo $dirs
 
 for d in "${dirs[@]}"
@@ -32,6 +35,8 @@ do
             xxd -p $midi | sed 's/\(ff5405.\{8\}\)../\100/' | xxd -r -p > $( dirname $midi )/temp # sed magic
             mv $( dirname $midi )/temp $midi
             rm -f $( dirname $midi )/temp
+            echo $midi
             python clip_midi.py -f $midi
         done
 done
+unset IFS
