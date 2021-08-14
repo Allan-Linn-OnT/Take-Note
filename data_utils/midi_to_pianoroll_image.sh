0c5fca48243cd2dae46bc8c5ae56472c79471c1e
Allan Lin <allan.lin2903@gmail.com>
2021-08-13 20:53:02 -0400

    data

---
+++ data_utils/midi_to_pianoroll_image.sh
@@ -0,0 +1,14 @@
+
+#!/usr/bin/env bash
+
+IFS=$'\n'
+for d in $( find ../data -maxdepth 2 -type d)
+do
+    dir=$( basename "$d" )
+    if [ $dir == "test" ]  ||  [ $dir == "train" ]  || [ $dir == "valid" ];
+    then
+        echo $d
+        python midi_to_pianoroll_image.py -m $d -o "$d"_image
+    fi
+done
+unset IFS
